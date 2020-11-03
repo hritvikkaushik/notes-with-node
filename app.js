cbefa6d440ee9ae8191e9470f3b81fcf59bf5c42
hritvikkaushik <hritvikkaushik@gmail.com>
2020-11-03 12:00:46 +0530

    implement add, remove, list, read. completed sec 4

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
-const getNotes = require('./notes');
-const validator = require ('validator');
-const chalk = require('chalk');
-console.log(getNotes());
-console.log(validator.isEmail("hritvik@gmail.com"));
-console.log(chalk.green("Success!"));
+const { string } = require('yargs');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: string
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: string
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body);
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Title of note to be removed',
+            demandOption: true,
+            type: string
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title);
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler() {
+        notes.listNotes();
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Title of note to be read',
+            demandOption: true,
+            type: string
+        }
+    },
+    handler(argv) {
+        notes.readNotes(argv.title);
+    }
+})
+
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,6 +1,72 @@
-const getNotes = function(){
-    const str = "Your notes...";
-    return str;
+const fs = require('fs');
+const chalk = require('chalk');
+
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch(e) {
+        return [];
+    }
+};
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+};
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNote = notes.find(note => note.title === title);
+    if (!duplicateNote){
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes);
+        console.log(chalk.green.inverse("Note added successfully!"));
+    } else {
+        console.log(chalk.red.inverse("Note title is duplicate!!!"));
+    }
+    
+};
+
+const removeNote = (title) => {
+    console.log("Title of note to be removed: " + title);
+    const notes =  loadNotes();
+    const newNotes = notes.filter((note) => note.title !== title);
+    if(newNotes.length === notes.length){
+        console.log(chalk.red.inverse("Note not found!!!"));
+    } else {
+        saveNotes(newNotes);
+        console.log(chalk.green.inverse("Note removed successfully!!!"));
+    }
+};
+
+const listNotes = () => {
+    console.log(chalk.cyanBright.inverse("Your notes:"));
+    const notes = loadNotes();
+    notes.forEach(note => {
+        console.log(note.title);
+    });
 }
 
-module.exports = getNotes;
+const readNotes = (title) => {
+    const notes = loadNotes();
+    const found = notes.find(note => note.title === title);
+    if (found){
+        console.log(chalk.green.inverse("Reading note..."));
+        console.log(chalk.bold(found.title));
+        console.log(found.body);
+    } else {
+        console.log (chalk.red.inverse("Note not found !!!"));
+    }
+}
+
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNotes: readNotes
+};
